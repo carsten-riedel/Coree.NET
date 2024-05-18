94c3a218d819d085e2f00008006a58a5da8fca54
Updated form Workflow <carstenriedel@outlook.com>
2024-05-18 10:52:00 +0200

    update

---
+++ src/Projects/Coree.NET/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coree.NET
+{
+    internal class Class1
+    {
+
+        public static void ssd()
+        {
+            TimeOnly ssd = new TimeOnly();  
+           
+        }
+    }
+}
